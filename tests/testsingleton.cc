3aae360409d2db542a893b3cb645fe41007065b8
Chow Loong Jin <hyperair@ubuntu.com>
2011-12-05 00:35:46 +0800

    Add test for singleton

---
+++ tests/testsingleton.cc
@@ -0,0 +1,38 @@
+#include <nihpp/singleton.hh>
+#include <cassert>
+
+namespace
+{
+    bool in_main;
+    bool constructed;
+
+    class TestSingleton : public nih::Singleton<TestSingleton>
+    {
+    public:
+        TestSingleton ()
+        {
+            assert (in_main);
+            assert (!constructed);
+
+            constructed = true;
+        }
+
+        ~TestSingleton ()
+        {
+            assert (!in_main);
+        }
+    };
+}
+
+int main ()
+{
+    in_main = true;
+    TestSingleton &first = TestSingleton::instance ();
+    TestSingleton &second = TestSingleton::instance ();
+
+    assert (&first == &second);
+
+    in_main = false;
+
+    return 0;
+}
